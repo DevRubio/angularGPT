c0fcd58b38fa796e4cd65f6812819fb50f776ed7
Oscar Rubio <ofrubio0@gmail.com>
2024-03-15 13:03:22 -0500

    (UPDATE) Se crea los alias

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ChatMessageComponent } from '../../components/chat-bubbles/chatMessage/chatMessage.component';
-import { MyMessagesComponent } from '../../components/chat-bubbles/myMessages/myMessages.component';
+import { ChatMessageComponent, MyMessagesComponent } from '@Components/index';
 
 @Component({
   selector: 'app-orthography-page',
