d7965b21fd47f2a67b73b927adaae03aaf2556f6
Oscar Rubio <ofrubio0@gmail.com>
2024-03-14 20:08:28 -0500

    (ADD) Se crean las rutas del proyecto

--- src/app/presentation/layouts/dashboardLayout/dashboardLayout.component.ts
+++ src/app/presentation/layouts/dashboardLayout/dashboardLayout.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-dasboard-layout',
+  selector: 'app-dashboard-layout',
   standalone: true,
   imports: [
     CommonModule,
+    RouterModule
   ],
-  templateUrl: './dasboardLayout.component.html',
+  templateUrl: './dashboardLayout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DasboardLayoutComponent { }
+export class DashboardLayoutComponent { }
